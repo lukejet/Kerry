dda993a6c9e3277bcbb707868b552999ef85c75a
lukejet <lukejet@163.com>
2020-04-10 17:11:35 +0800

    update update_ip.sh

--- update_ip.sh
+++ update_ip.sh
@@ -5,8 +5,15 @@ TIME=`date`
 cd ~/Kerry/
 git pull
 
-wget -qO- -t1 -T2 ipv4.icanhazip.com > ips.txt
-IP=`cat ips.txt`
+wget -qO- -t1 -T2 ipv4.icanhazip.com > tmp.txt
 
+IP=`cat tmp.txt`
+if [ -z "$IP"  ]; then
+	echo "The return IP is empty, will exit!"
+	exit 0
+fi
+
+#echo "return $IP "
+mv tmp.txt ips.txt
 git commit ips.txt  --message="update ip to $IP $TIME"
 git push origin master
